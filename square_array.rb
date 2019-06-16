aeb7d9bb7bd41c84ad906ba08b711f530d36f975
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-16 06:50:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,10 @@
 def square_array(array)
+  puts array.inspect
   array.each do |item|
     id = array.index item
     array[id] = item * item
-    puts array.index item
-    
-
   end
+  puts array.inspect
 end
 
 square_array([1,2,3])
